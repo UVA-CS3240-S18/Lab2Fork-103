792b9446f7e9b3d7a1ae01c26566c140e4e4e718
Ani Sridhar <asridhar2015@gmail.com>
2018-01-29 15:43:42 -0500

    modified closest_wendys.py with minor improvments

--- closest_wendys.py
+++ closest_wendys.py
@@ -7,7 +7,11 @@ google_maps_url = "https://www.google.com/maps/@35.372742,-81.954957,15z?hl=en"
 
 def distance_between(lat_1, lon_1, lat_2, lon_2):
     theta = lon_1 - lon_2
-    dist = math.sin(lat_1 * math.pi / 180.0) * math.sin(lat_2 * math.pi / 180.0) + math.cos(lat_1 * math.pi / 180.0) * math.cos(lat_2 * math.pi / 180.0) * math.cos(theta * math.pi / 180.0)
+    #Seperate some of the math out to make it easier to read and understand
+    dist_1 = math.sin(lat_1 * math.pi / 180.0) * math.sin(lat_2 * math.pi / 180.0)  
+    dist_2 = math.cos(lat_1 * math.pi / 180.0) * math.cos(lat_2 * math.pi / 180.0) * math.cos(theta * math.pi / 180.0)
+    #End of changes
+    dist = dist_1 + dist_2
     dist = math.acos(dist)
     dist = dist * 180.0 / math.pi
     dist = dist * 60 * 1.1515
