f1abbab9db6cc05ba5cd0c7ddf517862d85c0fe8
kvell36 <kvell36@gmail.com>
2018-01-29 16:09:48 -0500

    Closest Wendy's - Use Input
    Uses input instead of hardcoded values as previously used in the program.

--- closest_wendys.py
+++ closest_wendys.py
@@ -14,11 +14,12 @@ def distance_between(lat_1, lon_1, lat_2, lon_2):
 
     return dist
 
-#lat = float(input("Current latitude: "))
-#lon = float(input("Current longitude: "))
+#now uses user input instead of fixed values - Kevin Vellanki (kbv2fx)
+lat = float(input("Current latitude: "))
+lon = float(input("Current longitude: "))
 
-lat = 38.0322727
-lon = -78.50997339999999
+#lat = 38.0322727
+#lon = -78.50997339999999
 datafile = open("wendys.csv", "r")
 
 closest_dist = 200
