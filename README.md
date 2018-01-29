ad551e2e287fc2d2b6c451bbf88873a60da44012
krp9da <krp9da@virginia.edu>
2018-01-29 15:40:46 -0500

    Added transportation suggestions to Wendy's program

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Lab2Fork-103
 Repo that students will fork for Lab 2 for lab 103
+Added transportation suggest to Wendy's program.
--- closest_wendys.py
+++ closest_wendys.py
@@ -36,6 +36,13 @@ for line in datafile:
 datafile.close()
 
 print("The closest Wendy's (", closest_wendys, ") is", closest_dist, "miles away.")
+if (closest_dist < 2):
+	print ("You should run there!")
+elif (closest_dist < 10):
+	print ("Drive there!")
+else:
+	print ("Wendy's is always too far away... :(")
+
 google_maps_url = google_maps_url.replace(' ', '+')
 webbrowser.open(google_maps_url)
 
